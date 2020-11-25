6988fa8a500f1e6f7077639a5200f025ce7197d4
Anton Goroshko <radjahair@gmail.com>
2020-11-25 13:13:07 +1000

    Add User username before_save collback

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,19 +15,7 @@ class User < ApplicationRecord
   validates :password, confirmation: true, presence: true, on: :create
   validates :password_confirmation, presence: true, on: :create
 
-  before_save :encrypt_password
-
-  def encrypt_password
-    if self.password.present?
-      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
-
-      self.password_hash = User.hash_to_string(
-        OpenSSL::PKCS5.pbkdf2_hmac(
-          self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST
-        )
-      )
-    end
-  end
+  before_save :encrypt_password, :downcase_username
 
   def self.hash_to_string(password_hash)
     password_hash.unpack('H*')[0]
@@ -46,5 +34,25 @@ class User < ApplicationRecord
       )
 
     user.password_hash == hashed_password ? user : nil
+  end
+
+  private
+
+  def downcase_username
+    if self.username.present?
+      self.username.downcase!
+    end
+  end
+
+  def encrypt_password
+    if self.password.present?
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(
+          self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST
+        )
+      )
+    end
   end
 end
