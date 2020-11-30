7e80dfb57d75345b1cfc3be2624e388ae224d977
Anton Goroshko <radjahair@gmail.com>
2020-11-30 14:51:26 +1000

    Views refactoring

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def user_avatar(user)
-    user.avatar_url.present? ? user.avatar_url : asset_path 'avatar.jpg'
+    user.avatar_url.present? ? user.avatar_url : asset_path('avatar.jpg')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'users#index'
 
   resources :users
+  resources :questions
 
   get 'show' => 'users#show'
 end
