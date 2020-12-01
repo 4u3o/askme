fe2c91671c84e026e7f7c8b8d133cc3ca3485b67
Anton Goroshko <radjahair@gmail.com>
2020-12-01 20:13:50 +1000

    Change declension method to i18n gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @questions = @user.questions.order(created_at: :desc)
+    @questions = @user.questions.order(created_at: :desc) if @user&.questions
     @new_question = @user.questions.build
     @questions_count = @questions.count
     @answers_count = @questions.where.not(answer: nil).count
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,16 +3,6 @@ module ApplicationHelper
     user.avatar_url.present? ? user.avatar_url : asset_path('avatar.jpg')
   end
 
-  def declension(number, one, few, many)
-    remainder_10 = number % 10
-
-    return many if (11..14).include?(number % 100)
-    return one if remainder_10 == 1
-    return few if (2..4).include?(remainder_10)
-
-    many
-  end
-
   def fa_icon(icon_class)
     content_tag 'span', '', class: "fa fa-#{icon_class}"
   end
