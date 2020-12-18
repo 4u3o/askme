3b7268f5e0bc2862b5b1180cfbb958a14991c4da
Anton Goroshko <radjahair@gmail.com>
2020-12-18 10:19:05 +1000

    Move hashtag handles from questions_controller to question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,32 @@ class Question < ApplicationRecord
   has_and_belongs_to_many :hashtags # dependent: :destroy
 
   validates :text, presence: true, length: { maximum: 255 }
+
+  after_save :find_tags, :hashtags_update, :refresh_tags
+
+  def find_tags
+    question_text = [text, answer].join(' ')
+    @tags_after_update = question_text.scan(/#[\p{Word}-]+/i).uniq
+    @tags_after_update.map!(&:downcase)
+  end
+
+  def hashtags_update
+    tags_before_update = hashtags.pluck(:tag)
+
+    outdated_tags = hashtags.where(tag: tags_before_update - @tags_after_update)
+
+    hashtags.delete(outdated_tags)
+
+    new_tags = @tags_after_update - tags_before_update
+
+    if new_tags
+      new_tags.each do |tag|
+        hashtags << Hashtag.find_or_create_by(tag: tag)
+      end
+    end
+  end
+
+  def refresh_tags
+    Hashtag.left_joins(:questions).where(questions: { id: nil }).destroy_all
+  end
 end
